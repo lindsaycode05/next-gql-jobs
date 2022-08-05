139f1cb634c24307be17a50891252b01d2e417ba
Dragosh <dragoshcode@gmail.com>
2022-08-06 00:15:08 +0300

    Create the styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,28 +21,104 @@ interface IJob {
 
 const Home: NextPage<IJob> = ({ jobs }) => {
   return (
-    <Box>
+    <Box
+      width='92%'
+      marginX='auto'
+      marginY='8%'
+      display='flex'
+      flexDirection='column'
+      gap={9}
+    >
+      <Box>
+        <Typography variant='h3' textAlign='center' fontWeight='300'>
+          Jobs list using GraphQL&apos;s jobs API ✨
+        </Typography>
+        <Typography
+          variant='subtitle2'
+          textAlign='center'
+          color='#4a2512'
+          marginTop={1}
+        >
+          (created with passion by{' '}
+          <Link
+            sx={{ color: '#a1785c', textDecorationColor: '#a1785c' }}
+            href='https://github.com/lindsaycode05'
+            target='_blank'
+          >
+            Lindsay
+          </Link>{' '}
+          for{' '}
+          <Link
+            sx={{ color: '#a1785c', textDecorationColor: '#a1785c' }}
+            target='_blank'
+            href='https://jobbatical.com'
+          >
+            Jobbatical)
+          </Link>
+        </Typography>
+      </Box>
+
       {jobs.map((job) => (
-        <Box key={job.id} display='flex' alignItems='center'>
-          <Box display='flex' flexDirection='column'>
-            <Box display='flex' alignItems='center'>
-              <Chip label={job.isFeatured && 'FEATURED'} />
-              <Chip label={job.commitment.title} variant='outlined' />
+        <Box
+          key={job.id}
+          display='flex'
+          alignItems='center'
+          justifyContent='space-between'
+          paddingY={5}
+          paddingX={9}
+          borderRadius={10}
+          sx={{ backgroundColor: job.isFeatured ? '#ffe484ba' : '#faf0e6' }}
+        >
+          <Box display='flex' flexDirection='column' gap={4}>
+            <Box display='flex' alignItems='center' gap={1.2}>
+              {job.isFeatured && (
+                <Chip
+                  label={'FEATURED'}
+                  sx={{ backgroundColor: '#4a2512', color: '#faf0e6' }}
+                />
+              )}
+              <Chip
+                label={job.commitment.title}
+                variant='outlined'
+                sx={{ backgroundColor: '#a1785c', color: '#faf0e6' }}
+              />
             </Box>
-            <Avatar alt='company' src={job.company.logoUrl} />
+            <Box display='flex' alignItems='center' gap={3.2}>
+              <Avatar
+                sx={{ width: 70, height: 70 }}
+                alt='company'
+                src={job.company.logoUrl}
+              />
+              <Box display='flex' flexDirection='column'>
+                <Typography variant='h6'>{job.title}</Typography>
+                <Link
+                  target='_blank'
+                  href={job.company.websiteUrl}
+                  sx={{ color: '#4a2512', textDecorationColor: '#4a2512' }}
+                >
+                  {job.company.name}
+                </Link>
+              </Box>
+            </Box>
           </Box>
-          <Box display='flex' flexDirection='column'>
-            <Typography>{job.title}</Typography>
-            <Link target='_blank' href={job.company.websiteUrl}>
-              {job.company.name}
-            </Link>
-          </Box>
-          <Box display='flex' flexDirection='column'>
-            <Typography>{job.locationNames}</Typography>
-          </Box>
-          <Box>
-            <Link href={job.applyUrl}>Apply Now</Link>
-          </Box>
+          <Typography marginTop={5.5}>{job.locationNames}</Typography>
+          <Link
+            sx={{
+              backgroundColor: '#4a2512',
+              color: '#faf0e6',
+              padding: 1.3,
+              textDecoration: 'none',
+              borderRadius: 2,
+              transition: '.2s ease',
+              '&:hover': {
+                opacity: 0.9,
+              },
+            }}
+            href={job.applyUrl}
+            marginTop={4.4}
+          >
+            Apply Now
+          </Link>
         </Box>
       ))}
     </Box>
